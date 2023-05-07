f06b074b73abd2b3b7979add8b05da62d0e334df
Rui Lopes <76881129+RuiL1904@users.noreply.github.com>
2023-05-07 19:23:19 +0100

    Overall tweaks (#29)

--- src/monitor.c
+++ src/monitor.c
@@ -13,66 +13,55 @@
 #include "requests.h"
 #include "utils.h"
 
+// Default folder
 char *folder = "PIDS-folder";
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
   if (argc == 2) {
+    // Folder has been specified
     folder = argv[1];
   } else if (argc > 2) {
-    printf("Usage: %s [folder]\n", argv[0]);
+    printf("Usage: %s <folder>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
   // Create the folder if it doesn't exist
-  if (mkdir(folder, 0777) == -1) {
-    perror("mkdir");
-    exit(EXIT_FAILURE);
+  if (access(folder, F_OK) == -1) {
+    if (mkdir(folder, 0777) == -1) {
+      perror("mkdir");
+      exit(EXIT_FAILURE);
+    }
   }
 
-  REQUESTS_ARRAY *requests_array = create_requests_array(100);
-
-  if (mkfifo(MAIN_FIFO_NAME, 0666) ==
-      -1) {  // 0666 stands for read/write permissions for all users
-    perror("mkfifo");
-    exit(EXIT_FAILURE);
-  }
+  // Create the named pipe
+  make_fifo(MAIN_FIFO_NAME);
 
   printf("Monitor is running...\n");
 
   // Open the named pipe for reading
-  int fd = open(MAIN_FIFO_NAME, O_RDONLY);
-  if (fd == -1) {
-    perror("open");
-    exit(EXIT_FAILURE);
-  }
+  int fd;
+  open_fifo(&fd, MAIN_FIFO_NAME, O_RDONLY);
 
   // Open the named pipe for writing, in order to avoid the server being on hold
-  int fd2 = open(MAIN_FIFO_NAME, O_WRONLY);
-  if (fd2 == -1) {
-    perror("open");
-    exit(EXIT_FAILURE);
-  }
+  int fd2;
+  open_fifo(&fd2, MAIN_FIFO_NAME, O_WRONLY);
+
+  // Create the requests array with initial size of 100
+  REQUESTS_ARRAY *requests_array = create_requests_array(100);
 
   while (true) {
     // Read data from the named pipe
     PROGRAM_INFO *info = malloc(sizeof(PROGRAM_INFO));
     REQUEST_TYPE type = read_from_fd(fd, info, sizeof(PROGRAM_INFO));
 
-    deal_request(requests_array, info, type);
+    deal_with_request(requests_array, info, type);
 
     free(info);
   }
 
   // Close the named pipes
-  if (close(fd) == -1) {
-    perror("close");
-    exit(EXIT_FAILURE);
-  }
-
-  if (close(fd2) == -1) {
-    perror("close");
-    exit(EXIT_FAILURE);
-  }
+  close_fifo(fd);
+  close_fifo(fd2);
 
   // Delete named pipe file
   if (unlink(MAIN_FIFO_NAME) == -1) {
