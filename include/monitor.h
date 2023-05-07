b2615fcf52e4b9683669cf40fd7570b5d8a43a16
Rui Lopes <ruilopesm19@gmail.com>
2023-05-07 19:23:19 +0100

    Overall tweaks (#29)

--- include/monitor.h
+++ include/monitor.h
@@ -1,6 +1,7 @@
 #ifndef MONITOR_H
 #define MONITOR_H
 
+// Make folder accessible from other files
 extern char *folder;
 
 #endif  // MONITOR_H
--- include/tracer.h
+++ include/tracer.h
@@ -2,7 +2,7 @@
 #define TRACER_H
 
 int execute_program(char *full_program, char **parsed_program, int monitor_fd);
-int execute_status(int fd);
+int execute_status(int monitor_fd);
 int execute_pipeline(
     char *pipeline, char **parsed_pipeline, int pipeline_cmds_count,
     int monitor_fd
